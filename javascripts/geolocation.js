0954cc3cb4af4d0765df40606cd64f672e84fcb5
jleewebdev <lee45276@gmail.com>
2016-02-27 16:47:27 +0800

    Added map img and marker

--- javascripts/geolocation.js
+++ javascripts/geolocation.js
@@ -1,3 +1,26 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position);
-});
+var map = {
+  width: 600,
+  height: 420,
+  buildURL: function() {
+    var base = "http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=",
+        coords = this.position.coords.latitude + "," + this.position.coords.longitude;
+    base += this.width + "x" + this.height + "&center=" + coords;
+    return base + "&markers=" + coords;
+  },
+
+  buildImage: function() {
+    var $img = $("<img />", {
+      width: this.width,
+      height: this.height,
+      src: this.buildURL()
+    });
+    $(document.body).html($img);
+  },
+
+  build: function(position) {
+    this.position = position;
+    this.buildImage();
+  }
+}
+
+navigator.geolocation.getCurrentPosition(map.build.bind(map));
